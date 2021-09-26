4c4eb1bab38d4cee94180965f511605deb42f39b
ADITYA CHOUHAN <90592325+Adityachouhan76@users.noreply.github.com>
2021-09-26 22:01:49 +0530

    Add files via upload

---
+++ string 13g.c++.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	
+	string s1 = "nincompoop";
+	
+   // for(int i=0;i<s1.size();i++)
+  //  cout<<s1[i]<<endl;
+   string s = s1.substr(6,4);
+   cout<<s<<endl;
+  
+    return 0;
+} 
